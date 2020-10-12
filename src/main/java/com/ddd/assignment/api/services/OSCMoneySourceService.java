dd2eb453806143ce3678309a428e1a2e1d4f7a30
brian <yenpq.tech@gmail.com>
2020-10-12 22:34:05 +0700

    add some moneySource

---
+++ src/main/java/com/ddd/assignment/api/services/OSCMoneySourceService.java
@@ -0,0 +1,18 @@
+package com.ddd.assignment.api.services;
+
+import com.ddd.assignment.api.MoneySource;
+import com.ddd.assignment.api.interfaces.MoneySourceInterface;
+
+public class OSCMoneySourceService implements MoneySourceInterface {
+    public void credit(MoneySource moneySource, double amount) {
+
+    }
+
+    public void debit(MoneySource MoneySource, double amount) {
+
+    }
+
+    public MoneySource active() {
+        return null;
+    }
+}
---
+++ src/main/java/com/ddd/assignment/api/services/VTBMoneySourceService.java
@@ -0,0 +1,18 @@
+package com.ddd.assignment.api.services;
+
+import com.ddd.assignment.api.MoneySource;
+import com.ddd.assignment.api.interfaces.MoneySourceInterface;
+
+public class VTBMoneySourceService implements MoneySourceInterface {
+    public void credit(MoneySource moneySource, double amount) {
+
+    }
+
+    public void debit(MoneySource MoneySource, double amount) {
+
+    }
+
+    public MoneySource active() {
+        return null;
+    }
+}
